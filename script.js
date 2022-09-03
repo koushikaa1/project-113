7c28ccdaa8087d78fd599a7a8c6a661ff195d346
koushikaa1 <99191216+koushikaa1@users.noreply.github.com>
2022-09-03 10:48:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function preload(){
+
+}
+
+function setup()
+{
+    canvas = createCanvas(500,300);
+    canvas.center();
+}
+function draw(){
+    rect(30, 20, 55, 55, 10, 10, 10, 10);
+    rect(45, 75, 20,200);
+    rect(30,230 , 55, 55, 10, 10, 10, 10);
+    rect(409, 70, 20,200);
+    rect(390,230 , 55, 55, 10, 10, 10, 10);
+    rect(390, 20, 55, 55, 10, 10, 10, 10);
+    rect(85, 30,305, 20);
+    rect(85, 250,305, 20);
+
+}
+function take_snapshot()
+{
+    save('myImage.png');
+}
